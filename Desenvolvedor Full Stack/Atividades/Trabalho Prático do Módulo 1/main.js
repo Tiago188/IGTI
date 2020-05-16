003bf52e0075e11a84321a3d5c43620dfbd4b21a
Tiago S. Oliveira <TiagoProff@gmail.com>
2020-05-16 13:38:03 -0300

    color change by ranges

--- Desenvolvedor Full Stack/Atividades/Trabalho Prático do Módulo 1/main.js
+++ Desenvolvedor Full Stack/Atividades/Trabalho Prático do Módulo 1/main.js
@@ -3,6 +3,20 @@ document.addEventListener('DOMContentLoaded', appInit);
 function appInit() {
     const inputs  = document.querySelectorAll('.range');
     const amostra = document.querySelector('#amostra');
+
+    function colorChange(params) {
+        const inputRed   = inputs[0]; 
+        const inputGreen = inputs[1]; 
+        const inputBlue  = inputs[2];
+        const RGB = `(${inputRed.value}, ${inputGreen.value}, ${inputBlue.value})`;
+        
+        amostra.style.backgroundColor = `rgb${RGB}`;
+        console.log(`O código RGB é: RGB ${RGB}`);
+    }
+
+    document.addEventListener('colorchange', function (e) {
+        colorChange();
+    });
     
     inputs.forEach(input => {
         const fieldValue = `#${input.id}_value`;
@@ -11,8 +25,11 @@ function appInit() {
         input.fieldValue = document.querySelector(fieldValue);
         input.fieldValue.value = input.value;
 
+        colorChange();
+
         input.addEventListener('change', function () {
             this.fieldValue.value = this.value;
+            document.dispatchEvent( new Event('colorchange') );
         }); 
     });
 }
