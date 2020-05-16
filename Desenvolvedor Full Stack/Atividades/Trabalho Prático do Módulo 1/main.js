4f997a455671d4a877c71016935c1da2e0d28da9
Tiago S. Oliveira <TiagoProff@gmail.com>
2020-05-16 13:37:12 -0300

    event change

--- Desenvolvedor Full Stack/Atividades/Trabalho Prático do Módulo 1/main.js
+++ Desenvolvedor Full Stack/Atividades/Trabalho Prático do Módulo 1/main.js
@@ -3,12 +3,16 @@ document.addEventListener('DOMContentLoaded', appInit);
 function appInit() {
     const inputs  = document.querySelectorAll('.range');
     const amostra = document.querySelector('#amostra');
-
+    
     inputs.forEach(input => {
         const fieldValue = `#${input.id}_value`;
 
         input.value = Math.random() * 255;
         input.fieldValue = document.querySelector(fieldValue);
         input.fieldValue.value = input.value;
+
+        input.addEventListener('change', function () {
+            this.fieldValue.value = this.value;
+        }); 
     });
 }
