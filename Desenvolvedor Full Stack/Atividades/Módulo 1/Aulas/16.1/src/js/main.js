7dc7d6e05870d7da1bb17064e77510e79aec3482
Tiago S. Oliveira <TiagoProff@gmail.com>
2020-05-28 13:44:48 -0300

    Create main.js

---
+++ Desenvolvedor Full Stack/Atividades/Módulo 1/Aulas/16.1/src/js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    loadData();
+});
+
+function loadData(params) {
+    const rrgomideAPI = fetch('https://api.github.com/users/rrgomide')
+    .then( response => {
+        response.json()
+        .then( data => showUser(data) )
+        .catch(function (error) {
+            console.log(error);
+        });
+    })
+    .catch(function (error) {
+        console.log(error);
+    });   
+}
+
+function showUser(user) {
+    const divUser = document.querySelector('#user-data');
+
+    divUser.textContent = user.login + ' ' + user.name;
+}
